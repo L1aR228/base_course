d7b14228c50c4ba950ce0c02c30bc36b2ee43e9a
L1aR228 <dagadzanan432@gmail.com>
2024-09-25 19:03:22 +0000

    Home work

---
+++ task6.py
@@ -0,0 +1,15 @@
+age = input()
+gender = input()
+name = input()
+city = input()
+class_school = input()
+# list = [age, gender, name, city, class_school]
+# print(list)
+
+sp = []
+sp.append(age)
+sp.append(gender)
+sp.append(name)
+sp.append(city)
+sp.append(class_school)
+print(sp)
