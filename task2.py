3b9aa1b407650e967ec3b6123ec96fd32769d09a
L1aR228 <dagadzanan432@gmail.com>
2024-10-22 18:30:50 +0000

    Ксюша лох

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import constant as co
-v =  np.sqrt((10 * 100 * np.tan(35)**2) / 2 * (np.cos(45) ** 2) * (1 - np.tan(35) * np.tan(45)))
+v =  np.sqrt((10 * 100 * np.tan(35)**2) / (2 * (np.cos(45) ** 2) * (1 - np.tan(35) * np.tan(45))))
 print(v)
 
-n = 2 / (0.5 ** np.pi) * (co.h / (co.k * 200) ** (3/2)) * co.e ** (-300 / co.k * 200) * 300 ** (200 / 2)
+n = (2 / (0.5 ** np.pi)) * (co.h / (co.k * 200) ** (3/2)) * (co.e ** (-300 / (co.k * 200))) * (300 ** (200 / 2))
 print(n)
