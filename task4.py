e88016724883f0b199e765f738ed1bb491410058
L1aR228 <dagadzanan432@gmail.com>
2024-10-23 16:50:00 +0000

    ксюша лох

--- task4.py
+++ task4.py
@@ -1,10 +1,12 @@
 import numpy as np 
 N = int(input())
 M = int(input())
-trigonometry_array = np.ones((N, M))
-for i in trigonometry_array:
-    for j in trigonometry_array:
-        form = np.sin(N * i + M * j + 1)
-
+trigonometry_array = np.zeros((N, M))
 for i in range(N):
-    print(form)
+    for j in range(M):
+        trigonometry_array[i, j] = np.sin(N * i)
+        if trigonometry_array[i, j] < 0:
+            trigonometry_array = 0
+        else:
+            pass
+print(trigonometry_array)
