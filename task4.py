5cae3dd6cf56328935e22dda014e78290bfb450a
L1aR228 <dagadzanan432@gmail.com>
2024-10-29 20:22:28 +0000

    Ксюша лох

--- task4.py
+++ task4.py
@@ -1,12 +1,13 @@
-import numpy as np 
+import numpy as np
 N = int(input())
 M = int(input())
 trigonometry_array = np.zeros((N, M))
 for i in range(N):
     for j in range(M):
-        trigonometry_array[i, j] = np.sin(N * i)
-        if trigonometry_array[i, j] < 0:
-            trigonometry_array = 0
+        summ = np.sin(N * i + M * j + 1)
+        if summ < 0:
+            trigonometry_array[i, j] = 0
         else:
-            pass
+            trigonometry_array[i, j] = summ
 print(trigonometry_array)
+
